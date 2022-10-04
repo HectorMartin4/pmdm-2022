ea2e16220f1014795109e56ec3b624dd9743860c
Héctor Martín <hectormartin4.c@gmail.com>
2022-10-04 16:21:58 +0200

    Más ejercicios con SGVs

--- app/src/main/java/com/hmarcre/pmdm/ut01/ex02/presentation/Ut01Ex02bActivity.kt
+++ app/src/main/java/com/hmarcre/pmdm/ut01/ex02/presentation/Ut01Ex02bActivity.kt
@@ -2,6 +2,7 @@ package com.hmarcre.pmdm.ut01.ex02.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hmarcre.pmdm.R
 
 class Ut01Ex02bActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
