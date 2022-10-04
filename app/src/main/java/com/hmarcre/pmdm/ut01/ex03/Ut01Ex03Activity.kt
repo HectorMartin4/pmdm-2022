d3ace944645ace1e6a4cb09482f7f371c5976ecc
Héctor Martín <hectormartin4.c@gmail.com>
2022-10-04 17:16:29 +0200

    Ejercicios con RelativeLayout

---
+++ app/src/main/java/com/hmarcre/pmdm/ut01/ex03/Ut01Ex03Activity.kt
@@ -0,0 +1,12 @@
+package com.hmarcre.pmdm.ut01.ex03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hmarcre.pmdm.R
+
+class Ut01Ex03Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex03)
+    }
+}
