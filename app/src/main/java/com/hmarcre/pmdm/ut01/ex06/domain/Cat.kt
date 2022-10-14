ca5cd6c16e5d1afd50a12932d71dd2db5bf316a9
Héctor Martín <hectormartin4.c@gmail.com>
2022-10-14 18:37:29 +0200

    Creado Ejercicio 7

---
+++ app/src/main/java/com/hmarcre/pmdm/ut01/ex06/domain/Cat.kt
@@ -0,0 +1,3 @@
+package com.hmarcre.pmdm.ut01.ex06.domain
+
+data class Cat(val name: String, val surname: String)
---
+++ app/src/main/java/com/hmarcre/pmdm/ut01/ex06/presentation/Ut01Ex06Activity.kt
@@ -0,0 +1,33 @@
+package com.hmarcre.pmdm.ut01.ex06.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.hmarcre.pmdm.R
+import com.hmarcre.pmdm.ut01.ex06.domain.Cat
+
+class Ut01Ex06Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex06)
+
+        setupView()
+    }
+
+    private fun setupView(){
+        val inputName = findViewById<EditText>(R.id.inputName)
+        val inputSurname = findViewById<EditText>(R.id.inputSurname)
+        val actionSave = findViewById<Button>(R.id.actionSave)
+
+        actionSave.setOnClickListener{
+            val cat = Cat(
+                inputName.text.toString(),
+                inputSurname.text.toString()
+            )
+
+            Log.d("@dev", " Cat: $cat")
+        }
+    }
+}
