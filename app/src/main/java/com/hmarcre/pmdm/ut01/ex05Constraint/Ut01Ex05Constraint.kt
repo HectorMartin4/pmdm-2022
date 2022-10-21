50020baa3caecb05d70fddea31f8373329c8f6e9
Héctor Martín <hectormartin4.c@gmail.com>
2022-10-21 20:24:23 +0200

    Ejercicio 5 con Constraint Layout

---
+++ app/src/main/java/com/hmarcre/pmdm/ut01/ex05Constraint/Ut01Ex05Constraint.kt
@@ -0,0 +1,12 @@
+package com.hmarcre.pmdm.ut01.ex05Constraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hmarcre.pmdm.R
+
+class Ut01Ex05Constraint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex05_constraint)
+    }
+}
